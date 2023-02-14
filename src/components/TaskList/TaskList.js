f54fb8143f86b2005476daeb6c6ee3a535184057
Vasylij-Kashtalyan <88617002+Vasylij-Kashtalyan@users.noreply.github.com>
2023-02-14 20:17:20 +0200

    add constans

--- src/components/TaskList/TaskList.js
+++ src/components/TaskList/TaskList.js
@@ -1,10 +1,29 @@
-import { Task } from "../Task/Task";
 import css from "./TaskList.module.css";
+import { Task } from "../Task/Task";
+import { useSelector } from "react-redux";
+import { statusFilters } from "../../redux/constants";
+
+const getVisibleTasks = (tasks, statusFilter) => {
+  switch (statusFilter) {
+    case statusFilters.active:
+      return tasks.filter((task) => !task.completed);
+    case statusFilters.completed:
+      return tasks.filter((task) => task.completed);
+    default:
+      return tasks;
+  }
+};
 
 export const TaskList = () => {
+  const tasks = useSelector((state) => state.tasks);
+  const statusFilter = useSelector((state) => state.filters.status);
+
+  // Обчислюємо масив завдань, які необхідно відображати в інтерфейсі
+  const visibleTasks = getVisibleTasks(tasks, statusFilter);
+
   return (
     <ul className={css.list}>
-      {[].map((task) => (
+      {visibleTasks.map((task) => (
         <li className={css.listItem} key={task.id}>
           <Task task={task} />
         </li>
